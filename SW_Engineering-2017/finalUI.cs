1eb25291adad116819ece17243c08eb394cc7629
Druanae <aw932@student.anglia.ac.uk>
2017-10-30 01:05:36 +0000

    finalUI.cs: Added methods to show/hide each panel as necessary. finalUI.cs: Removed and added some buttons I felt were necessary. Notes:     Honestly I think we've massively overcomplicated this, there's no     reason we can't use an individual form for certain aspects such as     the the patient and schedule lookup results.

--- SW_Engineering-2017/finalUI.cs
+++ SW_Engineering-2017/finalUI.cs
@@ -15,6 +15,15 @@ namespace SW_Engineering_2017
         public finalUI()
         {
             InitializeComponent();
+            mainMenuPanel.Visible = false;
+            prescriptionPanel.Visible = false;
+            testResultSearchPanel.Visible = false;
+            staffScheduleSearchPanel.Visible = false;
+            changeStaffSchedulePanel.Visible = false;
+            newPatientPanel.Visible = false;
+            findPatientPanel.Visible = false;
+            editPatientPanel.Visible = false;
+            newAppointmentPanel.Visible = false;
         }
         //creates instance of Connection and DataSet
         Connection databaseCon;
@@ -25,7 +34,7 @@ namespace SW_Engineering_2017
         {
             //set the connection string
             string connectionString = Properties.Settings.Default.Connection;
-            
+
             //sends connection string to the database 
             databaseCon = new Connection(connectionString);
 
@@ -54,6 +63,169 @@ namespace SW_Engineering_2017
         {
             //close database connection 
             databaseCon.closeConnection();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                mainMenuPanel.Visible = true;
+                loginPanel.Visible = false;
+            }
+        }
+
+        private void logoutBtn_Click(object sender, EventArgs e)
+        {
+            if (!loginPanel.Visible)
+            {
+                mainMenuPanel.Visible = false;
+                loginPanel.Visible = true;
+            }
+        }
+
+        private void patientBtn_Click(object sender, EventArgs e)
+        {
+            if (!findPatientPanel.Visible)
+            {
+                findPatientPanel.Visible = true;
+                mainMenuPanel.Visible = false;
+            }
+        }
+
+        private void testResultsBtn_Click(object sender, EventArgs e)
+        {
+            if (!testResultSearchPanel.Visible)
+            {
+                testResultSearchPanel.Visible = true;
+                mainMenuPanel.Visible = false;
+            }
+        }
+
+        private void scheduleBtn_Click(object sender, EventArgs e)
+        {
+            if (!staffScheduleSearchPanel.Visible)
+            {
+                staffScheduleSearchPanel.Visible = true;
+                mainMenuPanel.Visible = false;
+            }
+        }
+
+        private void prescriptionBtn_Click(object sender, EventArgs e)
+        {
+            if (!prescriptionPanel.Visible)
+            {
+                prescriptionPanel.Visible = true;
+                mainMenuPanel.Visible = false;
+            }
+
+        }
+
+        private void btnPresMenu_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                prescriptionPanel.Visible = false;
+                mainMenuPanel.Visible = true;
+            }
+        }
+
+        private void btnPresCancel_Click(object sender, EventArgs e)
+        {
+            if (!findPatientPanel.Visible)
+            {
+                prescriptionPanel.Visible = false;
+                findPatientPanel.Visible = true;
+            }
+        }
+
+        private void MenuTestBNTRS_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                testResultSearchPanel.Visible = false;
+                mainMenuPanel.Visible = true;
+            }
+        }
+
+        private void MenuBNTSSS_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                staffScheduleSearchPanel.Visible = false;
+                mainMenuPanel.Visible = true;
+            }
+        }
+
+        private void MenuBNTCSS_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                changeStaffSchedulePanel.Visible = false;
+                mainMenuPanel.Visible = true;
+            }
+        }
+
+        private void mainMenu_FP_BT_Click(object sender, EventArgs e)
+        {
+            if (!mainMenuPanel.Visible)
+            {
+                findPatientPanel.Visible = false;
+                mainMenuPanel.Visible = true;
+            }
+        }
+
+        private void edit_FP_B_Click(object sender, EventArgs e)
+        {
+            if (!editPatientPanel.Visible)
+            {
+                editPatientPanel.Visible = true;
+                findPatientPanel.Visible = false;
+            }
+        }
+
+        private void cancel_EP_B_Click(object sender, EventArgs e)
+        {
+            if (!findPatientPanel.Visible)
+            {
+                findPatientPanel.Visible = true;
+                editPatientPanel.Visible = false;
+            }
+        }
+
+        private void confirm_EP_B_Click(object sender, EventArgs e)
+        {
+            if (!findPatientPanel.Visible)
+            {
+                findPatientPanel.Visible = true;
+                editPatientPanel.Visible = false;
+            }
+        }
+
+        private void newAppointment_FP_B_Click(object sender, EventArgs e)
+        {
+            if (!newAppointmentPanel.Visible)
+            {
+                newAppointmentPanel.Visible = true;
+                findPatientPanel.Visible = false;
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (!findPatientPanel.Visible)
+            {
+                newAppointmentPanel.Visible = false;
+                findPatientPanel.Visible = true;
+            }
+        }
+
+        private void newPrescriptions_FP_B_Click(object sender, EventArgs e)
+        {
+            if (!prescriptionPanel.Visible)
+            {
+                prescriptionPanel.Visible = true;
+                findPatientPanel.Visible = false;
+            }
         }
     }
 }
