95a45b87aa11f3334aa71e007fd2733eae994f59
MattThorndyke <32261791+MattThorndyke@users.noreply.github.com>
2017-11-06 10:15:54 +0000

    Added more Staff for testing

---
+++ SW_Engineering-2017/dbo.Staff.data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT [dbo].[Staff] ON
+INSERT INTO [dbo].[Staff] ([Staff_ID], [Firstname], [Surname], [Staff_Type], [Password]) VALUES (1, N'Beckkie', N'See', N'Receptionist', N'BeckkieDo')
+INSERT INTO [dbo].[Staff] ([Staff_ID], [Firstname], [Surname], [Staff_Type], [Password]) VALUES (2, N'Derek', N'HumbleBerry', N'GP', N'imHumble')
+INSERT INTO [dbo].[Staff] ([Staff_ID], [Firstname], [Surname], [Staff_Type], [Password]) VALUES (3, N'Tabath', N'Swindle', N'Nurse', N'sandWood')
+INSERT INTO [dbo].[Staff] ([Staff_ID], [Firstname], [Surname], [Staff_Type], [Password]) VALUES (4, N'Alan', N'Alan', N'Nurse', N'Steve!')
+INSERT INTO [dbo].[Staff] ([Staff_ID], [Firstname], [Surname], [Staff_Type], [Password]) VALUES (5, N'Steven', N'Stevenson', N'GP', N'I<3Roblox')
+SET IDENTITY_INSERT [dbo].[Staff] OFF
